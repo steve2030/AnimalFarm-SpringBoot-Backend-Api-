f338609b0cc9ab0c56fbc12d6b8835cfedcd7a2b
steve2030 <stevemighty2030@gmail.com>
2023-09-02 21:53:13 +0300

    modified the Goat Service

--- src/main/java/com/eclectics/animalfarm/Animals/Goats/GoatService.java
+++ src/main/java/com/eclectics/animalfarm/Animals/Goats/GoatService.java
@@ -1,4 +1,20 @@
 package com.eclectics.animalfarm.Animals.Goats;
 
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
 public class GoatService {
+    private List<Goat> goats= Arrays.asList(
+            new Goat (1, "Female", "Freshian",2300),
+            new Goat (2, "male", "Freshian",2300),
+            new Goat (3, "Female", "California Grey",2300),
+            new Goat (4, "male", "The Nottigham blue",2300),
+            new Goat (5, "male", "Kienyeji",2300)
+    );
+    public List<Goat>getGoats(){
+        return goats;
+    }
 }
