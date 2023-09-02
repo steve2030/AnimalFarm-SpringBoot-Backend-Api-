df84a08d27a6a8c83bc3d898dd04d9239a26893c
steve2030 <stevemighty2030@gmail.com>
2023-09-02 20:44:10 +0300

    Generated  construstor classes, getters and setters for the cow class

--- src/main/java/com/eclectics/animalfarm/Animals/Cows/Cow.java
+++ src/main/java/com/eclectics/animalfarm/Animals/Cows/Cow.java
@@ -1,5 +1,55 @@
 package com.eclectics.animalfarm.Animals.Cows;
 
 public class Cow {
-    private long
+    private long Id;
+    private String Breed;
+    private String colour;
+    private String age;
+
+//    constructor
+
+    public Cow(long id, String breed, String colour, String age) {
+        Id = id;
+        Breed = breed;
+        this.colour = colour;
+        this.age = age;
+    }
+
+//    getters and setters
+
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getBreed() {
+        return Breed;
+    }
+
+    public void setBreed(String breed) {
+        Breed = breed;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+//    to string
+
 }
