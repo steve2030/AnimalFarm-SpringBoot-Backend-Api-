5cfddbcbe5685b02685371f37f797b01aac50a36
steve2030 <stevemighty2030@gmail.com>
2023-09-02 21:21:58 +0300

    modified the cow class

--- src/main/java/com/eclectics/animalfarm/Animals/Cows/Cow.java
+++ src/main/java/com/eclectics/animalfarm/Animals/Cows/Cow.java
@@ -8,58 +8,14 @@ public class Cow {
 
 //    constructor
 
-    public Cow(long id, String breed, String colour, String age) {
-        Id = id;
-        Breed = breed;
-        this.colour = colour;
-        this.age = age;
+
     }
 
 //    getters and setters
 
 
-    public long getId() {
-        return Id;
-    }
-
-    public void setId(long id) {
-        Id = id;
-    }
-
-    public String getBreed() {
-        return Breed;
-    }
-
-    public void setBreed(String breed) {
-        Breed = breed;
-    }
-
-    public String getColour() {
-        return colour;
-    }
-
-    public void setColour(String colour) {
-        this.colour = colour;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public void setAge(String age) {
-        this.age = age;
-    }
-
 //    to string
 
 
-    @Override
-    public String toString() {
-        return "Cow{" +
-                "Id=" + Id +
-                ", Breed='" + Breed + '\'' +
-                ", colour='" + colour + '\'' +
-                ", age='" + age + '\'' +
-                '}';
-    }
+
 }
--- src/main/java/com/eclectics/animalfarm/Animals/Cows/CowService.java
+++ src/main/java/com/eclectics/animalfarm/Animals/Cows/CowService.java
@@ -8,10 +8,10 @@ import java.util.List;
 @Service
 public class CowService {
     private List<Cow> cows= Arrays.asList(
-            new Cow(1, "Freshian", "Black",10),
-            new Cow(2, "New Jersey", "Grey", "12"),
-            new Cow(3, "California red", "ReddishBrown", 6),
-            new Cow(4, "Zebu", "Black", "21")
+            new Cow(1, "Freshian", "Black",10)
+//            new Cow(2, "New Jersey", "Grey", "12"),
+//            new Cow(3, "California red", "ReddishBrown", 6),
+//            new Cow(4, "Zebu", "Black", "21")
 
     );
     public List<Cow>getCows(){
