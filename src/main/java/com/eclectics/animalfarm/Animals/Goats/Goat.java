46357e6bdcfe6a0416e1e77b0746bc9b300e4ebe
steve2030 <stevemighty2030@gmail.com>
2023-09-02 21:39:24 +0300

    generated gettre, settrs  and to_string for the Cow Class

--- src/main/java/com/eclectics/animalfarm/Animals/Goats/Goat.java
+++ src/main/java/com/eclectics/animalfarm/Animals/Goats/Goat.java
@@ -14,5 +14,48 @@ public class Goat {
         Breed = breed;
         this.quantity = quantity;
     }
+//getters and setters
 
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String gender) {
+        Gender = gender;
+    }
+
+    public String getBreed() {
+        return Breed;
+    }
+
+    public void setBreed(String breed) {
+        Breed = breed;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+//    tom string conversion
+
+    @Override
+    public String toString() {
+        return "Goat{" +
+                "Id=" + Id +
+                ", Gender='" + Gender + '\'' +
+                ", Breed='" + Breed + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
