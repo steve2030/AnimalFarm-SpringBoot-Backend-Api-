8aa4a405808baa7ce069d92f04eb86d6d1efdd19
steve2030 <stevemighty2030@gmail.com>
2023-09-02 22:02:17 +0300

    modified the Goat controller to perform Get Method

--- src/main/java/com/eclectics/animalfarm/Animals/Goats/GoatController.java
+++ src/main/java/com/eclectics/animalfarm/Animals/Goats/GoatController.java
@@ -4,7 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class GoatController {
+    @Autowired
+    private GoatService goatService;
+    @GetMapping(path = "goats")
+    public List<Goat>getGoats(){
+        return goatService.getGoats();
+    }
 
 }
