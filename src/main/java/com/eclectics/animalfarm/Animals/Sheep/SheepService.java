61ea9b547511c74faf45fb413c485a83a28c7f07
steve2030 <stevemighty2030@gmail.com>
2023-09-03 13:57:48 +0300

    generate a constructor, getters and setters and to string clasess for the method sheep class

--- src/main/java/com/eclectics/animalfarm/Animals/Sheep/SheepService.java
+++ src/main/java/com/eclectics/animalfarm/Animals/Sheep/SheepService.java
@@ -4,5 +4,49 @@ public class SheepService {
     private Long Id;
     private String Breed;
     private String colour;
+//    constructor
 
+    public SheepService(Long id, String breed, String colour) {
+        Id = id;
+        Breed = breed;
+        this.colour = colour;
+    }
+
+//getters &Setters
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getBreed() {
+        return Breed;
+    }
+
+    public void setBreed(String breed) {
+        Breed = breed;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+//    to string
+
+
+    @Override
+    public String toString() {
+        return "SheepService{" +
+                "Id=" + Id +
+                ", Breed='" + Breed + '\'' +
+                ", colour='" + colour + '\'' +
+                '}';
+    }
 }
